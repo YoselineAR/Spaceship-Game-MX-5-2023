1fbbfb3a168f3d651eff6c98b1ed6ad201ccfed1
YoselineAR <yoseline.alonso@fundacion-jala.org>
2023-06-16 07:37:30 -0600

    tarea 1
    derecha, arriba y abajo
    completado

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,18 +1,46 @@
 import pygame
-from game.utils.constants import SPACESHIP
+from game.utils.constants import SPACESHIP, SCREEN_WIDTH
 
 class Spaceship:
     WIDTH = 40
     HEIGTH = 60
+    X_POS = (SCREEN_WIDTH // 2)- WIDTH
+    Y_POS = 500
 
     def __init__(self):
         self.image = SPACESHIP
         self.image = pygame.transform.scale(self.image, (self.WIDTH, self.HEIGTH))
         self. rect = self.image.get_rect()
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS
 
-    def update(self):
-        pass
+
+    def update(self, user_input):
+        if user_input[pygame.K_LEFT]:
+            self.move_left()
+        if user_input[pygame.K_RIGHT]:
+            self.move_righ()
+        if user_input[pygame.K_UP]:
+            self.move_up()
+        if user_input[pygame.K_DOWN]:
+            self.move_down()
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+    def move_left(self):
+        if self.rect.left > 0:
+            self.rect.x -= 10
+
+    def move_righ(self):
+        if self.rect.right > 0:
+            self.rect.x += 10
+
+    def move_up(self):
+        if self.rect.left > 0:
+            self.rect.y -=10
+    
+    def move_down(self):
+        if self.rect.left > 0:
+            self.rect.y +=10
                 
